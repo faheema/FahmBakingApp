cef4618ed91e5a2f87c0e41fa77864a470b93826
faheem.u <faheem.fu@gmail.com>
2018-01-18 23:49:54 +0530

    cuts

--- app/src/main/java/com/ap/fahm/bakingapp/model/remotedata/EndPoints.java
+++ app/src/main/java/com/ap/fahm/bakingapp/model/remotedata/EndPoints.java
@@ -25,7 +25,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface EndPoints {
-
     @GET("topher/2017/May/59121517_baking/baking.json")
     Call<ArrayList<Recipe>> getRecipes();
 
